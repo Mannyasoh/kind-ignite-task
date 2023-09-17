f1778dfc10042b57713c0063b32a5a9ccdadc617
Mannyasoh <121736518+Mannyasoh@users.noreply.github.com>
2023-09-17 02:17:19 -0400

    Updated kind-deploy.sh

--- scripts/kind-deploy.sh
+++ scripts/kind-deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-kind create cluster --config=./k8s/kind-config.yaml --name kind --wait 10s
+kind create cluster --config=./kind-config.yaml --name kind --wait 10s
 docker container ls --format "table {{.Image}}\t{{.State}}\t{{.Names}}"
 
